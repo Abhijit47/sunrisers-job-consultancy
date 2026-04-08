9c0bb4702b07df0bbeb1599dec2f70817374983c
Abhijit47 <akarmakar846@gmail.com>
2026-04-08 18:24:05 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,63 @@
+import Layout from '@/components/layout/Layout';
+
+// import About from '@/components/sections/home1/About';
+// import Banner from '@/components/sections/home1/Banner';
+// import Category from '@/components/sections/home1/Category';
+// import Chooseus from '@/components/sections/home1/Chooseus';
+// import Clients from '@/components/sections/home1/Clients';
+// import Funfact from '@/components/sections/home1/Funfact';
+// import Industries from '@/components/sections/home1/Industries';
+// import News from '@/components/sections/home1/News';
+// import Process from '@/components/sections/home1/Process';
+// import Slide_Text from '@/components/sections/home1/Slide-Text';
+// import Subscribe from '@/components/sections/home1/Subscribe';
+// import Team from '@/components/sections/home1/Team';
+
+import About3 from '@/components/sections/home3/About';
+import Apps3 from '@/components/sections/home3/Apps';
+import Banner3 from '@/components/sections/home3/Banner';
+import Category3 from '@/components/sections/home3/Category';
+import Chooseus3 from '@/components/sections/home3/Chooseus';
+import Clients3 from '@/components/sections/home3/Clients';
+import Download3 from '@/components/sections/home3/Download';
+import Dueal3 from '@/components/sections/home3/Dueal';
+import Industries3 from '@/components/sections/home3/Industries';
+import News3 from '@/components/sections/home3/News';
+import Slide_Text3 from '@/components/sections/home3/Slide-text';
+import Testimonial3 from '@/components/sections/home3/Testimonial';
+import Welcome3 from '@/components/sections/home3/Welcome';
+
+export default function Home() {
+  return (
+    <div className='boxed_wrapper'>
+      <Layout headerStyle={3} footerStyle={3}>
+        <Banner3 />
+        <Chooseus3 />
+        <Clients3 />
+        <About3 />
+        <Category3 />
+        <Download3 />
+        <Industries3 />
+        <Welcome3 />
+        <Slide_Text3 />
+        <Testimonial3 />
+        <Dueal3 />
+        <News3 />
+        <Apps3 />
+
+        {/* <Banner />
+        <Clients />
+        <About />
+        <Funfact />
+        <Slide_Text />
+        <Chooseus />
+        <Category />
+        <Industries />
+        <Process />
+        <Team />
+        <News />
+        <Subscribe /> */}
+      </Layout>
+    </div>
+  );
+}
---
+++ components/layout/Layout.js
@@ -0,0 +1,161 @@
+'use client';
+import { useWOW, WOWProvider } from '@moondev/next-wow';
+import { useEffect, useState } from 'react';
+import BackToTop from '../elements/BackToTop';
+import DataBg from '../elements/DataBg';
+import Breadcrumb from './Breadcrumb';
+import SearchPopup from './SearchPopup';
+import Sidebar from './Sidebar';
+import Footer1 from './footer/Footer1';
+import Footer2 from './footer/Footer2';
+import Footer3 from './footer/Footer3';
+import Footer4 from './footer/Footer4';
+import Footer5 from './footer/Footer5';
+import Header1 from './header/Header1';
+import Header2 from './header/Header2';
+import Header3 from './header/Header3';
+import Header4 from './header/Header4';
+import Header5 from './header/Header5';
+
+export default function Layout({
+  headerStyle,
+  footerStyle,
+  breadcrumbTitle,
+  children,
+  wrapperCls,
+}) {
+  const [scroll, setScroll] = useState(0);
+  // Mobile Menu
+  const [isMobileMenu, setMobileMenu] = useState(false);
+  const handleMobileMenu = () => {
+    setMobileMenu(!isMobileMenu);
+    !isMobileMenu
+      ? document.body.classList.add('mobile-menu-visible')
+      : document.body.classList.remove('mobile-menu-visible');
+  };
+
+  // Popup
+  const [isPopup, setPopup] = useState(false);
+  const handlePopup = () => setPopup(!isPopup);
+
+  // Sidebar
+  const [isSidebar, setSidebar] = useState(false);
+  const handleSidebar = () => setSidebar(!isSidebar);
+
+  useWOW(); // auto-init
+  // useWOW({ animateClass: 'animate__animated' });
+
+  useEffect(() => {
+    // if (typeof window === 'undefined') return;
+    // // const WOW = require('wowjs');
+    // import('wowjs').then((WOW) => {
+    //   window.wow = new WOW({
+    //     live: true,
+    //     mobile: true,
+    //     animateClass: 'animated',
+    //   });
+    //   window.wow.init();
+    // });
+
+    // window.wow = new WOW.WOW({
+    //   live: false,
+    // });
+
+    // window.wow = new Wow();
+    // window.wow.init();
+
+    document.addEventListener('scroll', () => {
+      const scrollCheck = window.scrollY > 100;
+      if (scrollCheck !== scroll) {
+        setScroll(scrollCheck);
+      }
+    });
+  }, []);
+
+  return (
+    <WOWProvider>
+      <DataBg />
+      <div className={`page-wrapper ${wrapperCls ? wrapperCls : ''}`} id='#top'>
+        {!headerStyle && (
+          <Header1
+            scroll={scroll}
+            isMobileMenu={isMobileMenu}
+            handleMobileMenu={handleMobileMenu}
+            handlePopup={handlePopup}
+            isSidebar={isSidebar}
+            handleSidebar={handleSidebar}
+          />
+        )}
+        {headerStyle == 1 ? (
+          <Header1
+            scroll={scroll}
+            isMobileMenu={isMobileMenu}
+            handleMobileMenu={handleMobileMenu}
+            handlePopup={handlePopup}
+            isSidebar={isSidebar}
+            handleSidebar={handleSidebar}
+          />
+        ) : null}
+        {headerStyle == 2 ? (
+          <Header2
+            scroll={scroll}
+            isMobileMenu={isMobileMenu}
+            handleMobileMenu={handleMobileMenu}
+            handlePopup={handlePopup}
+            isSidebar={isSidebar}
+            handleSidebar={handleSidebar}
+          />
+        ) : null}
+        {headerStyle == 3 ? (
+          <Header3
+            scroll={scroll}
+            isMobileMenu={isMobileMenu}
+            handleMobileMenu={handleMobileMenu}
+            handlePopup={handlePopup}
+            isSidebar={isSidebar}
+            handleSidebar={handleSidebar}
+          />
+        ) : null}
+        {headerStyle == 4 ? (
+          <Header4
+            scroll={scroll}
+            isMobileMenu={isMobileMenu}
+            handleMobileMenu={handleMobileMenu}
+            handlePopup={handlePopup}
+            isSidebar={isSidebar}
+            handleSidebar={handleSidebar}
+          />
+        ) : null}
+        {headerStyle == 5 ? (
+          <Header5
+            scroll={scroll}
+            isMobileMenu={isMobileMenu}
+            handleMobileMenu={handleMobileMenu}
+            handlePopup={handlePopup}
+            isSidebar={isSidebar}
+            handleSidebar={handleSidebar}
+          />
+        ) : null}
+
+        <Sidebar isSidebar={isSidebar} handleSidebar={handleSidebar} />
+        <SearchPopup isPopup={isPopup} handlePopup={handlePopup} />
+
+        {breadcrumbTitle && <Breadcrumb breadcrumbTitle={breadcrumbTitle} />}
+
+        {children}
+
+        {!footerStyle && <Footer1 />}
+        {footerStyle == 1 ? <Footer1 /> : null}
+        {!footerStyle && <Footer2 />}
+        {footerStyle == 2 ? <Footer2 /> : null}
+        {!footerStyle && <Footer3 />}
+        {footerStyle == 3 ? <Footer3 /> : null}
+        {!footerStyle && <Footer4 />}
+        {footerStyle == 4 ? <Footer4 /> : null}
+        {!footerStyle && <Footer5 />}
+        {footerStyle == 5 ? <Footer5 /> : null}
+      </div>
+      <BackToTop scroll={scroll} />
+    </WOWProvider>
+  );
+}
